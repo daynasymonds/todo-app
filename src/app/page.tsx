c259c454d6f60d5bce4cc82ea1ecaf01086586b0
Dayna Symonds <dayna@local>
2025-04-07 14:46:54 +1000

    Refactor LandingPage component to handle authentication state; separate signed in and signed out views

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,31 @@
-"use client";
-
-import { SessionProvider } from "next-auth/react";
 import Panel from "@/app/ui/Panel";
 import LandingPage from "@/src/app/ui/LandingPage";
 
-export default function App() {
+export default async function App() {
   return (
-    <SessionProvider>
-      <div className="flex h-screen">
-        <section className="grid grid-cols-1 gap-8 md:grid-cols-2 items-center text-center m-auto max-w-[650px] md:max-w-[1200px]">
+    <div className="flex h-screen">
+      <section className="grid grid-cols-1 gap-8 md:grid-cols-2 items-center text-center m-auto max-w-[650px] md:max-w-[1200px]">
+        <div>
+          <h1 className="text-2xl">Demonstration of a ToDo application</h1>
+          <p>
+            <small>
+              similar to{" "}
+              <a
+                className="underline"
+                target="_blank"
+                href={"https://workspace.google.com/products/keep/"}
+              >
+                Google Keep
+              </a>
+            </small>
+          </p>
+          <p>
+            <small>written using React + Tailwind CSS + NextJS</small>
+          </p>
           <LandingPage />
-          <Panel />
-        </section>
-      </div>
-    </SessionProvider>
+        </div>
+        {/* <Panel /> */}
+      </section>
+    </div>
   );
 }
--- src/app/ui/LandingPage.tsx
+++ src/app/ui/LandingPage.tsx
@@ -1,50 +1,41 @@
 import Link from "next/link";
 import LogoutForm from "@/app/ui/LogoutForm";
-import { useSession } from "next-auth/react";
+import { auth } from "@/auth";
 
-export default function LandingPage() {
-  const { data: session } = useSession();
-  const user = session?.user;
+export default async function LandingPage() {
+  const session = await auth();
+  if (!session) {
+    return <SignedOutLandingPage />;
+  }
+  return <SignedInLandingPage />;
+}
+
+function SignedOutLandingPage() {
   return (
-    <div>
-      <h1 className="text-2xl">Demonstration of a ToDo application</h1>
+    <div className="flex flex-col gap-4 py-4">
+      <p>This panel demonstrates a static React component.</p>
       <p>
-        <small>
-          similar to{" "}
-          <a
-            className="underline"
-            target="_blank"
-            href={"https://workspace.google.com/products/keep/"}
-          >
-            Google Keep
-          </a>
-        </small>
+        <Link className="underline hover:font-bold" href={"/login"}>
+          Log in
+        </Link>{" "}
+        or{" "}
+        <Link className="underline hover:font-bold" href={"/signup"}>
+          create an account
+        </Link>{" "}
+        to save your changes.
       </p>
-      <p>
-        <small>written using React + Tailwind CSS + NextJS</small>
-      </p>
-      {!user && (
-        <div className="flex flex-col gap-4 py-4">
-          <p>This panel demonstrates a static React component.</p>
-          <p>
-            <Link className="underline hover:font-bold" href={"/login"}>
-              Log in
-            </Link>{" "}
-            or{" "}
-            <Link className="underline hover:font-bold" href={"/signup"}>
-              create an account
-            </Link>{" "}
-            to save your changes.
-          </p>
-        </div>
-      )}
+    </div>
+  );
+}
 
-      {user && (
-        <div className="flex flex-col gap-4 py-4">
-            <p>This panel demonstrates a stateful and persisted React component. Changes you make here will be available the next time you log in.</p>
-          <LogoutForm />
-        </div>
-      )}
+function SignedInLandingPage() {
+  return (
+    <div className="flex flex-col gap-4 py-4">
+      <p>
+        This panel demonstrates a stateful and persisted React component.
+        Changes you make here will be available the next time you log in.
+      </p>
+      <LogoutForm />
     </div>
   );
 }
