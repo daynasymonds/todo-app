0e7bb3a3f883b7865f6bd5ae41b25f21083b9ffa
Dayna Symonds <dayna@local>
2025-04-11 13:27:29 +1000

    Update database SSL configuration to use 'prefer' or 'require' based on environment variable

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,7 +1,7 @@
 import postgres from 'postgres'
 
 const sql = postgres(process.env.POSTGRES_URL!, {
-  ssl: false,
+  ssl: process.env.POSTGRES_SSL === "false" ? "prefer" : "require",
   idle_timeout: 20,
   max_lifetime: 60 * 30,
 });
