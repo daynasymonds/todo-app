45e2f139092fe1619b0d7571155d4f7171270bf2
Dayna Symonds <dayna@local>
2025-04-25 12:28:15 +1000

    Refactor GET method in truncate API endpoint to use async/await for better error handling and readability

--- src/app/api/truncate/route.ts
+++ src/app/api/truncate/route.ts
@@ -1,6 +1,6 @@
 import sql from "@/app/lib/db";
 
-export function GET(request: Request) {
+export async function GET(request: Request) {
   console.log("Truncate API called");
   const authHeader = request.headers.get("authorization");
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
@@ -11,14 +11,15 @@ export function GET(request: Request) {
   }
 
   console.log("Truncating task_lists and users tables");
-  sql`TRUNCATE TABLE task_lists, users CASCADE`.then((result) => {
+
+  try {
+    const result = await sql`TRUNCATE TABLE task_lists, users CASCADE`;
     console.log("Truncated task_lists and users tables", result);
-  }).catch((error) => {
+    return new Response("ok", { status: 200 });
+  } catch (error) {
     console.error("Error truncating task_lists and users tables", error);
     return new Response("Error truncating tables", {
       status: 500,
     });
-  });
-  console.log("Truncate API completed successfully");
-  return new Response("ok", { status: 200 });
+  }
 }
