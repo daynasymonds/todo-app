9aaf8af5c622e8e92a4e5ef9bff6ec14baa96426
Dayna Symonds <dayna@local>
2025-04-25 12:15:41 +1000

    Enhance logging and response status in truncate API endpoint

--- src/app/api/truncate/route.ts
+++ src/app/api/truncate/route.ts
@@ -1,15 +1,18 @@
 import sql from "@/app/lib/db";
 
 export function GET(request: Request) {
+  console.log("Truncate API called");
   const authHeader = request.headers.get("authorization");
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    console.log("Unauthorized access attempt to truncate");
     return new Response("Unauthorized", {
       status: 401,
     });
   }
 
+  console.log("Truncating task_lists and users tables");
   sql`TRUNCATE TABLE task_lists, users CASCADE`.then((result) => {
     console.log("Truncated task_lists and users tables", result);
   });
-  return new Response("ok");
+  return new Response("ok", { status: 200 });
 }
